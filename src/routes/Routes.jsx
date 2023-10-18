90224584f9dbf98126ad567888a9420950a12670
ParthaMalakar <parthamalakar4321@gmail.com>
2023-10-18 16:13:33 +0600

    routes layout created

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,10 +1,20 @@
 
-const Routes = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
-
-export default Routes;
+import {
+    createBrowserRouter,
+    
+  } from "react-router-dom";
+import Root from "../layout/Root";
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Root />,
+    //   errorElement: <ErrorPage />,
+      children: [
+        {
+          path: "/",
+          element: ,
+        },
+      ],
+    },
+  ]);
+export default router;

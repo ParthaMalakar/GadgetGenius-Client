1e5dcc851dd9f82a4a04daecea61a362298cba47
ParthaMalakar <parthamalakar4321@gmail.com>
2023-10-18 18:00:37 +0600

    footer section created using daisyUi footer section

--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar/Navbar";
+import Footer from "../components/Footer/Footer";
 
 const Root = () => {
     return (
@@ -7,7 +8,9 @@ const Root = () => {
             <div  className="max-w-7xl mx-auto">
             <Navbar></Navbar>
             <Outlet></Outlet>
+            
             </div>
+            <Footer></Footer>
         </div>
     );
 };
